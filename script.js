829ec4bf4a2ff3ce35ac2e3c8abe6ba7b8e11d19
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-02-16 09:31:47 -0600

    If else for prompts, alerts, & confirms.

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword () {
+  console.log("Is this thing on?")
+  var characters = prompt("How many characters?")
+  if (characters >= 8 && characters <= 128) {
+    console.log("This is working.")
+    var wantLowerCase = confirm("Would you like to include lowercase letters?")
+    if (wantLowerCase === true) {
+      console.log("yes")
+      var wantUpperCase = confirm("Would you like to include uppercase letters?")
+    }
+  } else {
+    alert("Password length must be at least 8 characters, and no more 128 characters.")
+  }
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
