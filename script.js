1bac261a486b1a8aedf0439d9bb9c3247fded8c2
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-02-17 05:06:09 -0600

    If/else for all prompts/confirms.

--- script.js
+++ script.js
@@ -2,21 +2,44 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword () {
-  console.log("Is this thing on?")
+  // console.log("Is this thing on?")
   var characters = prompt("How many characters?")
   if (characters >= 8 && characters <= 128) {
     console.log("This is working.")
-    var wantLowerCase = prompt("Would you like to include lowercase letters?")
-    if (wantLowerCase === "Yes" || "Y") {
-      console.log("yes")
-      var wantUpperCase = prompt("Would you like to include uppercase letters?")
-      if (wantUpperCase === "Yes" || "Y") {
-        var wantNumeric = prompt("Would you like to include numeric values?")
-      }
-    }
   } else {
     alert("Password length must be at least 8 characters, and no more 128 characters. Please try again.")
+    return
   }
+  var passwordLength = characters;
+  console.log(passwordLength);
+
+  var wantLowerCase = confirm("Would you like to include lowercase letters?")
+    if (wantLowerCase === true){
+      console.log("Lowercase?: Yes")
+    } else {
+      console.log("Lowercase?: No")
+    }
+  
+  var wantUpperCase = confirm("Would you like to include uppercase letters?")
+    if (wantUpperCase === true){
+      console.log("Uppercase?: Yes")
+    } else {
+      console.log("Uppercase?: No")
+    }
+
+  var wantNumeric = confirm("Would you like to include numeric values?")
+    if (wantNumeric === true){
+      console.log("Numeric?: Yes")
+    } else {
+      console.log("Numeric?: No")
+    }
+
+  var wantSpecial = confirm("Would you like to include special characters?")
+    if (wantSpecial === true){
+      console.log("Special?: Yes")
+    } else {
+      console.log("Special?: No")
+    }
 }
 
 // Write password to the #password input
